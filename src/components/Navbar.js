2014e2eba96b66b20a41d9523b577bf3ec11bb9a
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-30 11:08:47 +0600

    set up Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,14 @@ import Temple from '../assets/temple.svg'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
       <ul>
         <li className='logo'>
           <img src={Temple} alt="logo" />
-          <span>TheProjectManagement</span>
+          <span>ProjectManagement</span>
         </li>
         <li>
-          <Link to='/login'>Login</Link>
+          <Link exact to='/login'>Login</Link>
         </li>
         <li>
           <Link to='/signup'>Signup</Link>

4f24c9bd35157468762d844fdc33544ff264aff6
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-30 10:30:30 +0600

    firebase init

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,3 +18,11 @@ firebase.initializeApp(firebaseConfig)
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
 
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export {
+  projectFirestore,
+  projectAuth,
+  timestamp
+}

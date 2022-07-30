02f5f5ef8cfbc8c61578452f830d2fbbbacb9a6d
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-30 12:23:55 +0600

    enable useSignup hook to handle image uploads

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { projectAuth } from '../firebase/config'
+import { projectAuth, projectStorage } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
@@ -8,24 +8,29 @@ export const useSignup = () => {
   const [isPending, setIsPending] = useState(false)
   const { dispatch } = useAuthContext()
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null)
     setIsPending(true)
 
     try {
-      // signup
+      // signup a user
       const res = await projectAuth.createUserWithEmailAndPassword(email, password)
-
       if (!res) {
         throw new Error('Could not complete signup')
       }
 
+      // upload user thumbnail
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`
+      const img = await projectStorage.ref(uploadPath).put(thumbnail)
+      const imgUrl = await img.ref.getDownloadURL()
+
       // add display name to user
-      await res.user.updateProfile({ displayName })
+      await res.user.updateProfile({ displayName, photoURL: imgUrl })
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
 
+      // update state as long as the component has not unmounted
       if (!isCancelled) {
         setIsPending(false)
         setError(null)

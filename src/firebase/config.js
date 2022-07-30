41ba9678fd068224faa8b3409a5b51d4f54beb31
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-30 12:12:51 +0600

    init firebase storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
+import "firebase/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLvFuxcAGL2gA19lTUZfOajehQE1XIOF4",
@@ -14,13 +15,15 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-// Initialize Firestore, Auth & Timestamp
+// Initialize Firebase services (Firestore, Storage, Auth, Timestamp)
 const projectFirestore = firebase.firestore()
 const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
 const timestamp = firebase.firestore.Timestamp
 
 export {
   projectFirestore,
   projectAuth,
+  projectStorage,
   timestamp
 }

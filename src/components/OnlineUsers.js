b42ceb5f01c599b14c7f60b2bc47e190f4a3120a
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-30 21:12:48 +0600

    ...

--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -14,14 +14,12 @@ const OnlineUsers = () => {
     <div className='user-list'>
       <h2>All Users</h2>
       {error && <div className='error'>{error}</div>}
-      {documents && documents.map(user => {
-        return (
-          <div key={user.id} className="user-list-item">
-            <span>{user.displayName}</span>
-            <Avatar src={user.photoURL} />
-          </div>
-        )
-      })}
+      {documents && documents.map(user => (
+        <div key={user.id} className="user-list-item">
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL} />
+        </div>
+      ))}
     </div>
   )
 }

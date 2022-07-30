e2ad752155b7e7840724aa1028d6fb89396be8c4
Iftee1711402 <tawsifhaque97@gmail.com>
2022-07-30 11:20:12 +0600

    set up Signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // styles
 import './Signup.css'
 
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null)
+
   return (
-    <div>Signup</div>
+    <form className='auth-form'>
+      <h2>Signup</h2>
+
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        <span>User name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        <span>Profile Thumbnail:</span>
+        <input
+          type="file"
+          required
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
   )
 }
 

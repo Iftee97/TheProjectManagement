5488dbd3035fffb3fe75016550b1aceb4677b954
Iftee1711402 <tawsifhaque97@gmail.com>
2022-08-01 00:07:11 +0600

    deleting a project by the project creator

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -2,20 +2,23 @@ import React from 'react'
 import Avatar from '../../components/Avatar'
 import { useFirestore } from '../../hooks/useFirestore'
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { useHistory } from 'react-router-dom'
 
 const ProjectSummary = ({ project }) => {
   const { deleteDocument } = useFirestore('projects')
   const { user } = useAuthContext()
+  const history = useHistory()
 
   const handleClick = (e) => {
-    e.preventDefault()
     deleteDocument(project.id)
+    history.push('/') // push to the dashboard page
   }
 
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className='due-date'>Project Due By: {project.dueDate.toDate().toDateString()}</p>
         <p className='details'>{project.details}</p>
         <h4>Project is assigned to:</h4>
@@ -27,7 +30,7 @@ const ProjectSummary = ({ project }) => {
           ))}
         </div>
       </div>
-      {user.id === project.createdBy.id && (
+      {user.uid === project.createdBy.id && (
         <button className='btn' onClick={handleClick}>Mark as Complete</button>
       )}
     </div>
